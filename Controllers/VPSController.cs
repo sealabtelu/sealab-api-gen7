3794f6e3d78d63e917cf310acdf612a1d07f0374
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2023-10-12 14:05:43 +0700

    feat: Deploy API

---
+++ Controllers/VPSController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SealabAPI.Base;
+using SealabAPI.DataAccess.Entities;
+using SealabAPI.DataAccess.Models.Constants;
+using SealabAPI.DataAccess.Models;
+using SealabAPI.DataAccess.Services;
+using Microsoft.AspNetCore.Authorization;
+using System.Diagnostics;
+
+namespace SealabAPI.Controllers
+{
+    [Route("[controller]")]
+    [Authorize(Roles = "Assistant")]
+    [ApiController]
+    public class VPSController : ControllerBase
+    {
+        private readonly ILogger<VPSController> _logger;
+        public VPSController(ILogger<VPSController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("deploy")]
+        public ActionResult Deploy()
+        {
+            try
+            {
+                string scriptPath = Path.Combine(Directory.GetCurrentDirectory(), "deploy.sh");
+                Process process = new()
+                {
+                    StartInfo = new()
+                    {
+                        FileName = "bash",
+                        Arguments = scriptPath,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+                process.Start();
+                process.WaitForExit();
+                string output = process.StandardOutput.ReadToEnd().Replace("\n", "");
+                process.Close();
+
+                return new SuccessApiResponse(string.Format(MessageConstant.Success), output);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorApiResponse(ex.InnerException == null ? ex.Message : ex.InnerException.Message);
+            }
+        }
+    }
+}
