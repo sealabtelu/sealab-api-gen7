d58d38638d5531693c6e3f7e491f7b82e8a934b2
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2023-10-14 21:26:44 +0700

    fix: reformat timestamp

--- DataAccess/Services/PreliminaryAssignmentAnswerService.cs
+++ DataAccess/Services/PreliminaryAssignmentAnswerService.cs
@@ -21,7 +21,7 @@ namespace SealabAPI.DataAccess.Services
                 CreatePreliminaryAssignmentAnswerRequest answerModel = model as CreatePreliminaryAssignmentAnswerRequest;
                 Module module = await _appDbContext.Set<Module>().Where(module => module.Id == answerModel.IdModule).AsNoTracking().FirstOrDefaultAsync();
                 Student student = await _appDbContext.Set<Student>().Include("User").Where(student => student.Id == answerModel.IdStudent).AsNoTracking().FirstOrDefaultAsync();
-                entity.FilePath = $"TP{module.SeelabsId}_{student.Day}_{student.Shift}_{student.Group}_{student.User.Nim}_{student.User.Name}_{((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds()}";
+                entity.FilePath = $"TP{module.SeelabsId}_{student.Day}_{student.Shift}_{student.Group}_{student.User.Nim}_{((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds()}_{student.User.Name}";
                 entity.SubmitTime = DateTime.Now;
             }
 
