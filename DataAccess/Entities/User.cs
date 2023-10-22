6c498a7257c87552588d2fb53fd150a8306fe2b6
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2023-10-22 10:10:48 +0700

    fix: phone whitespace

--- DataAccess/Entities/User.cs
+++ DataAccess/Entities/User.cs
@@ -8,6 +8,7 @@ namespace SealabAPI.DataAccess.Entities
         private string _username;
         private string _password;
         private string _name;
+        private string _phone;
         public string Nim { get; set; }
         public string Email { get; set; }
         public string Name { get => _name; set => _name = value?.ToTitleCase(); }
@@ -25,7 +26,7 @@ namespace SealabAPI.DataAccess.Entities
         }
         public string Password { get => _password; set => _password = _password == null ? value?.HashPassword() : value; }
         public string Role { get; set; }
-        public string Phone { get; set; }
+        public string Phone { get => _phone; set => _phone = value.Replace(" ", ""); }
         public string AppToken { get; set; }
     }
 }
