14a1d308188f2f37fc29243d60275f0417ccf83a
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2023-11-01 04:10:53 +0700

    fix: BAP parse date

--- DataAccess/Models/Seelabs/API/BAP/SeelabsBAPResponse.cs
+++ DataAccess/Models/Seelabs/API/BAP/SeelabsBAPResponse.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using AngleSharp.Dom;
 using SealabAPI.Base;
 using SealabAPI.DataAccess.Entities;
@@ -6,12 +7,13 @@ namespace SealabAPI.DataAccess.Models
 {
     public class SeelabsBAPResponse : BaseModel
     {
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
         public string Shift { get; set; }
         public string Module { get; set; }
         public SeelabsBAPResponse(IElement td)
         {
-            Date = td.Children[1].TextContent;
+
+            Date = DateTime.ParseExact(td.Children[1].TextContent, "dd/MMMM/yyyy", CultureInfo.InvariantCulture);
             Shift = td.Children[2].TextContent;
             Module = td.Children[3].TextContent;
         }
