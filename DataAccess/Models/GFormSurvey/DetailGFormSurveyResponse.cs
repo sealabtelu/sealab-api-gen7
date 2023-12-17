d8c436ef29309f592be9b09b61cdfc25e4a355f9
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2023-12-17 22:24:46 +0700

    feat: detail form

--- DataAccess/Models/GFormSurvey/DetailGFormSurveyResponse.cs
+++ DataAccess/Models/GFormSurvey/DetailGFormSurveyResponse.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Dynamic;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 using SealabAPI.Base;
 using SealabAPI.DataAccess.Entities;
 using SealabAPI.Helpers;
@@ -10,5 +12,19 @@ namespace SealabAPI.DataAccess.Models
         public Guid Id { get; set; }
         public string IdUser { get; set; }
         public dynamic Response { get; set; }
+        public DetailStudentResponse StudentInfo { get; set; } = new();
+        public DetailGFormSurveyResponse()
+        {
+            IncludeProperty(new string[] { "User" });
+        }
+        public override void MapToModel<TEntity>(TEntity entity)
+        {
+            if (entity is GFormSurvey gForm)
+            {
+                base.MapToModel(gForm);
+                StudentInfo.MapToModel(gForm.User.Student);
+                Response = JsonConvert.DeserializeObject<ExpandoObject>(gForm.Response);
+            }
+        }
     }
 }
